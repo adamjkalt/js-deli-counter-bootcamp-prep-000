fd5cd2fccdd0dfcc5deda062934ba3ff83fda1f4
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-09 05:30:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function currentLine (katzDeliLine) {
     return `The line is currently empty.`;
   }
   else (katzDeliLine > 0.5); {
-    return `The line is currently: 1. (katzDeliLine(0)), 2. (katzDeliLine(0)), 3. (katzDeliLine(0))`;
+    return `The line is currently: 1. ${katzDeliLine(0)}, 2. ${katzDeliLine(1)}, 3. ${katzDeliLine(2)}`;
     }
   }
